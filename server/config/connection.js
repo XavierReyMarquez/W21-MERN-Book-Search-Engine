1fe37780018c41ca18fde9bd19548866a64b8596
Xavier_Marquez <xavier.ray.marquez@gmail.com>
2022-08-02 18:17:44 -0700

    fxing issues with routes

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Books", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1/books", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
