cb8a0f7a0b43032bc9cdb0320d11df4a1f1dee7f
Xavier_Marquez <xavier.ray.marquez@gmail.com>
2022-08-02 18:49:14 -0700

    deploying to heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1/books", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/Books", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
